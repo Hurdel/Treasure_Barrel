68f9c26dd22fc09abf3f659b6e59dc3edbd6a3a7
Hurdel <108236599+Hurdel@users.noreply.github.com>
2023-04-17 00:00:13 +0200

    v0.1

--- src/main/java/tv/master_of_spirit/treasure_barrel/data/place_treasurebarrel.java
+++ src/main/java/tv/master_of_spirit/treasure_barrel/data/place_treasurebarrel.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Barrel;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
 import org.bukkit.inventory.meta.tags.ItemTagType;
 import org.bukkit.loot.LootContext;
 import org.bukkit.loot.LootTables;
@@ -16,12 +17,14 @@ public class place_treasurebarrel implements Listener {
     public void PlaceBlockEvent(BlockPlaceEvent event) {
         if (event.getBlockPlaced().getType().equals(Material.BARREL)) {
             if (event.getItemInHand().hasItemMeta()) {
-                if (event.getItemInHand().getItemMeta().getCustomTagContainer().getCustomTag(Treasure_barrel.getPlugin(Treasure_barrel.class).key, ItemTagType.STRING).equals("treasure_barrel")) {
-                    Barrel barrel = (Barrel) event.getBlock().getState();
-                    // create LootContext
-                    LootContext lootContext = new LootContext.Builder(event.getBlockPlaced().getLocation()).build();
-                    // fill the barrel with the loot
-                    LootTables.BURIED_TREASURE.getLootTable().fillInventory(barrel.getInventory(), null, lootContext);
+                if (event.getItemInHand().getItemMeta().getCustomTagContainer().getCustomTag(Treasure_barrel.getPlugin(Treasure_barrel.class).key, ItemTagType.STRING) != null) {
+                    if (event.getItemInHand().getItemMeta().getCustomTagContainer().getCustomTag(Treasure_barrel.getPlugin(Treasure_barrel.class).key, ItemTagType.STRING).equals("treasure_barrel")) {
+                        Barrel barrel = (Barrel) event.getBlock().getState();
+                        // create LootContext
+                        LootContext lootContext = new LootContext.Builder(event.getBlock().getLocation()).build();
+                        // fill the barrel with the loot
+                        LootTables.BURIED_TREASURE.getLootTable().fillInventory(barrel.getInventory(), null, lootContext);
+                    }
                 }
             }
         }
